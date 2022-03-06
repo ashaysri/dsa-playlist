f4a8c813162021684a5028d8011a9ff5ca697df2
Ashay Srivastava <56813891+ashnfire@users.noreply.github.com>
2022-03-06 21:50:53 +0530

    Time: 19 ms (48.21%), Space: 7.8 MB (46.85%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
+    unordered_map<char, int> hash {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
     int romanToInt(string s) {
-        unordered_map<char, int> hash {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         int idx = s.length()-1;
         int sum {hash[s[idx--]]};
         while(idx>=0){
